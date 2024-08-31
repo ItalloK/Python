1cc0296ec96fcea8294bc85dd1123039e815954c
ItalloK <itallo016@gmail.com>
2024-08-30 23:16:13 -0300

    Aula 38

---
+++ Aulas/Aula 31 a 40/Aula 38/Aula38.py
@@ -0,0 +1,17 @@
+import json
+
+#{
+#    "Nome":"Italo",
+#    "Time":"Aviadores",
+#    "Vivo":"True",
+#    "Energia":100,
+#    "Mochila":["Pederneira","Corda","Linha","Arame"],
+#    "Aeronaves":[
+#        {"Tipo" : "Transporte", "Habilidade" : 80},
+#        {"Tipo" : "Ataque", "Habilidade" : 100},
+#        {"Tipo" : "Reconhecimento", "Habilidade" : 50}
+#    ]
+#}
+
+jogador_j='{"Nome":"Italo", "Time":"Aviadores", "Vivo":"True", "Energia":100, "Mochila":"", }'
+
